bfcde87de6297514fb5249a677b4564b8745d8b0
Deepesh76780 <da76780@gmail.com>
2023-06-03 17:21:43 +0530

    client-setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  paths: {
+    artifacts: "./client/src/artifacts",
+  },
 };
+
