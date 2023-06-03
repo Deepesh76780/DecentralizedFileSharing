9336c6d9a61900d09c4b6e907f4a538269544301
Deepesh76780 <da76780@gmail.com>
2023-06-03 20:03:37 +0530

    contract_connection_AndCss

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,77 @@
-import "./App.css";
-import { useEffect, useState } from "react";
+import Upload from "./artifacts/contracts/Upload.sol/Upload.json";
+import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import Upload from "../src/artifacts/contracts/Upload.sol/Upload.json";
 import FileUpload from "./components/FileUpload";
-import Modal from "./components/Display";
 import Display from "./components/Display";
+import Modal from "./components/Modal";
+import "./App.css";
 
 function App() {
   const [account, setAccount] = useState("");
   const [contract, setContract] = useState(null);
   const [provider, setProvider] = useState(null);
-  const [modalOpen, setModalOpen] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const wallet = async () => {
+
+    const loadProvider = async () => {
       if (provider) {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload();
+        });
+
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
         await provider.send("eth_requestAccounts", []);
         const signer = provider.getSigner();
         const address = await signer.getAddress();
-        console.log(address);
         setAccount(address);
-
         let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
         const contract = new ethers.Contract(
           contractAddress,
           Upload.abi,
           signer
         );
-
-        console.log(contract);
+        //console.log(contract);
         setContract(contract);
-        setProvider(signer);
+        setProvider(provider);
       } else {
-        alert("metaMask Not Installed");
+        console.error("Metamask is not installed");
       }
     };
-
-    provider && wallet();
+    provider && loadProvider();
   }, []);
-
   return (
-    <div className="App">
-      <h1>Gdrive 3.0</h1>
-      <div class="bg"></div>
-      <div class="bg bg2"></div>
-      <div class="bg bg3"></div>
-      <p></p>
-      <FileUpload />
-      <Display />
-    </div>
+    <>
+      {!modalOpen && (
+        <button className="share" onClick={() => setModalOpen(true)}>
+          Share
+        </button>
+      )}
+      {modalOpen && (
+        <Modal setModalOpen={setModalOpen} contract={contract}></Modal>
+      )}
+
+      <div className="App">
+        <h1 style={{ color: "white" }}>Gdrive 3.0</h1>
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+
+        <p style={{ color: "white" }}>
+          Account : {account ? account : "Not connected"}
+        </p>
+        <FileUpload
+          account={account}
+          provider={provider}
+          contract={contract}
+        ></FileUpload>
+        <Display contract={contract} account={account}></Display>
+      </div>
+    </>
   );
 }
 
--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,7 +1,70 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import "./FileUpload.css";
+const FileUpload = ({ contract, account, provider }) => {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("No image selected");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (file) {
+      try {
+        const formData = new FormData();
+        formData.append("file", file);
 
-const FileUpload = () => {
-  return <div>FileUpload</div>;
+        const resFile = await axios({
+          method: "post",
+          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          data: formData,
+          headers: {
+            pinata_api_key: `21d6a4cb15475f5c30d2`,
+            pinata_secret_api_key: `ae367276ac83a10ff7ecc70b97616480454607cc0e7d10c025b4183c9310a48d`,
+            "Content-Type": "multipart/form-data",
+          },
+        });
+
+        const ImgHash = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+        contract.add(account, ImgHash);
+        alert("Successfully Image Uploaded");
+        setFileName("No image selected");
+        setFile(null);
+      } catch (e) {
+        alert("Unable to upload image to Pinata");
+      }
+    }
+    alert("Successfully Image Uploaded");
+    setFileName("No image selected");
+    setFile(null);
+  };
+  const retrieveFile = (e) => {
+    const data = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      setFile(e.target.files[0]);
+    };
+    console.log(e.target.files[0].name);
+    setFileName(e.target.files[0].name);
+    e.preventDefault();
+  };
+  return (
+    <div className="top">
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="file-upload" className="choose">
+          Choose Image
+        </label>
+        <input
+          disabled={!account}
+          type="file"
+          id="file-upload"
+          name="data"
+          onChange={retrieveFile}
+        />
+        <span className="textArea">Image: {fileName}</span>
+        <button type="submit" className="upload" disabled={!file}>
+          Upload File
+        </button>
+      </form>
+    </div>
+  );
 };
-
 export default FileUpload;
